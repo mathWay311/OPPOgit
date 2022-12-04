591f92d561dafe9c8cd6069a731c7a0e8bf53280
mathWay311 <matvej0311@gmail.com>
2022-12-04 16:41:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <iostream>
 
-double my_pow(double value, unsigned int power)
-{
-    double val = value;
-    for (int i = 1; i < power; i++) val *= value;
-    return val;
+double my_pow(double num, unsigned int deg) {
+    long result = 1;
+    while(deg) {
+        if (deg % 2 == 0) {
+            deg /= 2;
+            num *= num;
+        }
+        else {
+            deg--;
+            result *= num;
+        }
+    }
+
+    return result;
 }
 
 int main()
